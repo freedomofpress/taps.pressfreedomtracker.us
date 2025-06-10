6d123e8654b88479244bc6f4c69937d7871b5aa1
Harris Bard Lapiroff <harris@chromamine.com>
2025-06-10 16:17:27 -0400

    Continue preact conversion:
    - Move styles back to WebC file to prevent them from bloating page size when they're serialized inline
    - Remove post-full-template webc file

--- src/preact/post-full.js
+++ src/preact/post-full.js
@@ -13,62 +13,6 @@ const PostFull = ({ post, horizontal }) => {
     return 'short'
   }
 
-  const articleStyle = {
-    '--avatar-size': '60px',
-    '--padding': 'var(--line-height-base)',
-    '--metadata-width': '50%',
-    marginBottom: 'var(--line-height-base)',
-    border: '1px solid var(--color-border)',
-    borderRadius: '10px',
-    display: 'grid',
-    gridTemplateColumns: horizontal
-      ? 'calc(var(--avatar-size) + var(--padding)) auto calc(var(--metadata-width) - var(--padding))'
-      : 'calc(var(--avatar-size) + var(--padding)) 1fr',
-    gridTemplateAreas: horizontal
-      ? '"avatar author metadata" "avatar content metadata"'
-      : '"avatar author" "avatar content" "metadata metadata"',
-    gap: '0 var(--line-height-base)',
-    overflow: 'hidden',
-    boxShadow: '0 2px 3px rgba(0, 0, 0, 0.05)'
-  }
-
-  const avatarStyle = {
-    gridArea: 'avatar',
-    width: 'var(--avatar-size)',
-    borderRadius: '4px',
-    marginTop: 'var(--padding)',
-    marginLeft: 'var(--padding)'
-  }
-
-  const authorStyle = {
-    gridArea: 'author',
-    paddingTop: 'var(--padding)',
-    paddingRight: 'var(--padding)',
-    paddingBottom: 'calc(0.5 * var(--padding))'
-  }
-
-  const contentStyle = {
-    gridArea: 'content',
-    fontSize: getContentSizeClass(post.content) === 'long'
-      ? 'var(--font-size-base)'
-      : getContentSizeClass(post.content) === 'medium'
-        ? 'calc(1.5 * var(--font-size-base))'
-        : 'calc(2 * var(--font-size-base))',
-    paddingRight: 'var(--padding)',
-    paddingBottom: 'var(--padding)'
-  }
-
-  const metadataStyle = {
-    gridArea: 'metadata',
-    background: 'var(--color-background-accent)',
-    padding: 'var(--padding)'
-  }
-
-  const linkStyle = {
-    color: 'var(--color-muted)',
-    textDecoration: 'none'
-  }
-
   const formattedDate = new Date(post.date).toLocaleString('en-US', {
     month: 'long',
     day: 'numeric',
@@ -76,21 +20,23 @@ const PostFull = ({ post, horizontal }) => {
   })
 
   return html`
-    <article style=${articleStyle} data-hash=${post.hash}>
-      <img style=${avatarStyle} src="/media/avatar.jpeg" alt="Avatar" />
-      <div style=${authorStyle}>
-        <div style=${{ fontWeight: 'bold' }}>Donald J. Trump</div>
-        <div style=${{ color: 'var(--color-muted)' }}>@realDonaldTrump</div>
+    <article class="post" data-hash=${post.hash} data-horizontal=${horizontal}>
+      <img class="post-avatar" src="/media/avatar.jpeg" alt="Avatar" />
+      <div class="post-author">
+        <div class="post-author-name">Donald J. Trump</div>
+        <div class="post-author-handle">@realDonaldTrump</div>
       </div>
-      <div style=${contentStyle}>${post.content}</div>
-      <div style=${metadataStyle}>
+      <div class="post-content" data-size=${getContentSizeClass(post.content)}>
+        ${post.content}
+      </div>
+      <div class="post-metadata">
         <${MetadataGrid}>
           <dt>Posted</dt>
           <dd>
-            <a style=${linkStyle} href=${`/post/${post.hash}/`}>
+            <a class="post-link" href=${`/post/${post.hash}/`}>
               <time datetime=${post.dateString}>${formattedDate}</time>
             </a>
-            (<a style=${linkStyle} href=${post.link}>${post.platform}</a>)
+            (<a class="post-link" href=${post.link}>${post.platform}</a>)
           </dd>
           ${post.primaryTarget && html`
             <dt>Target</dt>
--- src/preact/post-tag.js
+++ src/preact/post-tag.js
@@ -2,41 +2,9 @@ import { h } from 'preact'
 import { html } from 'htm/preact'
 
 const PostTag = ({ name }) => {
-  const style = {
-    display: 'inline-block',
-    backgroundColor: 'var(--color-pft-yellow-light)',
-    padding: '0.2rem 0.4rem',
-    marginLeft: '0.4rem',
-    marginBottom: '0.3rem',
-    textDecoration: 'none',
-    color: 'var(--color-base)',
-    position: 'relative',
-    paddingLeft: '1.2em'
-  }
-
-  const hashStyle = {
-    position: 'absolute',
-    left: '0.4rem',
-    color: 'var(--color-muted-more)'
-  }
-
-  const linkStyle = {
-    textDecoration: 'none',
-    color: 'inherit'
-  }
-
-  const handleMouseEnter = (e) => {
-    e.currentTarget.style.backgroundColor = 'var(--color-pft-yellow)'
-  }
-
-  const handleMouseLeave = (e) => {
-    e.currentTarget.style.backgroundColor = 'var(--color-pft-yellow-light)'
-  }
-
   return html`
-    <span style=${style} onMouseEnter=${handleMouseEnter} onMouseLeave=${handleMouseLeave}>
-      <span style=${hashStyle}>#</span>
-      <a href="#" style=${linkStyle}>${name}</a>
+    <span class="post-tag">
+      <a href="#" class="post-tag-link">${name}</a>
     </span>
   `
 }
