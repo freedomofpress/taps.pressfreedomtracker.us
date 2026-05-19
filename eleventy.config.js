0a9d4ab91bfe0c97715bb3ca6767c1441828a514
Ethan Paul <ethan@freedom.press>
2026-05-19 10:48:34 -0400

    Update build config to directly copy static headers file
    Headers file must be in the root of the build output directory

--- eleventy.config.js
+++ eleventy.config.js
@@ -62,6 +62,8 @@ export default async function(eleventyConfig) {
     })
 
     eleventyConfig.addPassthroughCopy("src/media")
+
+    eleventyConfig.addPassthroughCopy("src/_headers")
 }
 
 export const config = {
